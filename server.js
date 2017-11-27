98a888e47667fa6f54eca563a693090f705f22ee
Erlend Oftedal <erlend@oftedal.no>
2017-11-27 09:49:48 +0100

    testing secrets

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 var express = require("express");
 var fs = require("fs");
 
-var secrets = fs.readFileSync("/etc/test-secret/config", "utf-8");
-var config = JSON.parse(secrets);
 
 
 
@@ -14,6 +12,8 @@ var app = new express();
 
 
 app.get("/", (req,res) => {
+	var secrets = fs.readFileSync("/etc/test-secret/config", "utf-8");
+	var config = JSON.parse(secrets);
 	res.status(200).end("Yellow world: " + config.password);
 });
 
