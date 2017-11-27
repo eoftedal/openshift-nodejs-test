7d67b44ae5b7ac10529df6d4972050a8b46f60dd
Erlend Oftedal <erlend@oftedal.no>
2017-11-27 10:16:59 +0100

    config map

--- server.js
+++ server.js
@@ -12,9 +12,11 @@ var app = new express();
 
 
 app.get("/", (req,res) => {
-	var secrets = fs.readFileSync("/etc/test-secret/config", "utf-8");
-	var config = JSON.parse(secrets);
-	res.status(200).end("Yellow world: " + config.password);
+	var map = fs.readFileSync("/data/testnode-mapping", "utf-8");
+	var config = JSON.parse(map);
+
+
+	res.status(200).end("Yellow world: " + JSON.stringify(config));
 });
 
 app.listen(8080);
