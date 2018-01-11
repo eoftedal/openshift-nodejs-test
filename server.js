defdba2c6d93a3213d3d33eca227aa6d70d0bf3e
Erlend Oftedal <erlend@oftedal.no>
2018-01-11 08:34:57 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,22 +1,14 @@
 var express = require("express");
 var fs = require("fs");
 
-
-
-
 var app = new express();
-
-
-
-
-
 
 app.get("/", (req,res) => {
 	var map = fs.readFileSync("/data/testnode-mapping", "utf-8");
 	var config = JSON.parse(map);
+	res.header("Content-Type", "text/plain");
 
-
-	res.status(200).end("Yellow world: " + JSON.stringify(config));
+	res.status(200).end("Yellow world: \n\n" + JSON.stringify(config) + "\n\n" + JSON.stringify(req.headers));
 });
 
 app.listen(8080);
