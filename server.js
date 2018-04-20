4b500090e4c7a5e905a60f676a2a41c13610597f
Erlend Oftedal <erlend@oftedal.no>
2018-04-20 13:07:17 +0200

    Include address in response

--- server.js
+++ server.js
@@ -5,8 +5,7 @@ var app = new express();
 
 app.get("/", (req,res) => {
 	res.header("Content-Type", "text/plain");
-
-	res.status(200).end("Yellow world: \n\n" + JSON.stringify(req.headers));
+	res.status(200).end("Yellow world: " + req.socket.getAddress().address + "\n\n" + JSON.stringify(req.headers));
 });
 
 function log(msg) {
