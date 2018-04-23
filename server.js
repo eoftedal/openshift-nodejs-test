0edee4e5225084c203b9dee9c51af172d2f3a370
Erlend Oftedal <erlend@oftedal.no>
2018-04-23 07:56:25 +0200

    More logging

--- server.js
+++ server.js
@@ -4,14 +4,36 @@ var fs = require("fs");
 var app = new express();
 
 app.get("/", (req,res) => {
+	let address = req.socket.address().address;
+	console.log(new Date() + " " + address + " " + req.method + " " + req.url + " " + req.headers["user-agent"]);
 	res.header("Content-Type", "text/plain");
 
-	res.status(200).end("Yellow world: " + req.socket.address().address + "\n\n" + JSON.stringify(req.headers));
+	res.status(200).end("Yellow world: " + address + "\n\n" + JSON.stringify(req.headers));
 });
 
 function log(msg) {
 	console.log(new Date() + " : " + msg);
 }
+
+process.on('beforeExit', () => {
+	console.log('before exit');
+});
+process.on('exit', (code) => {
+	console.log('exit with ' + code);
+});
+process.on('warning', (warning) => {
+  console.warn(warning.name);    // Print the warning name
+  console.warn(warning.message); // Print the warning message
+  console.warn(warning.stack);   // Print the stack trace
+});
+
+function handle(signal) {
+  console.log(`Received ${signal}`);
+  process.exit(0);
+}
+process.on('SIGINT', handle);
+process.on('SIGTERM', handle);
+
 
 function start() {
 	log("Planning to listen...")
